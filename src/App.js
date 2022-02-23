1244161e0714a81828b9b752f925e1fbf4f8d021
“Deyvin <deyvinvelasquez0202@gmail.com>
2022-02-22 20:46:05 -0500

    trying to add auth

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Routes, Route } from 'react-router-dom';
 import { Layout } from 'antd';
 import Main from "./components/Main";
 import "./App.css"
 
 function App() {
   return (
+  <>
     <Layout>
       <Layout.Header className='Header'>
         To-do List
@@ -13,6 +15,12 @@ function App() {
     </Layout.Content>
     <Layout.Footer className='Footer'> &copy; 2022, Boca Code </Layout.Footer>
     </Layout>
+
+    <Routes>
+      <Route path='/login' element={<Login setuser={setUser} user={user} />} />
+    </Routes>
+
+   </>  
   );
 }
 
---
+++ src/UserLogin/Login.js
@@ -0,0 +1,6 @@
+import { useState, useEffect } from "react";
+
+
+function Login({user, setUser}){
+    const [email, setEmail] = useState();
+}
